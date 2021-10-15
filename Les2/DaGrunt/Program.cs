3d64c716270a8ede4ee2ef277a0a45c5e304bf5f
Jayden <macbeanjayden@gmail.com>
2021-10-15 10:45:42 +0200

    Grunt added

---
+++ Les2/DaGrunt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DaGrunt
+{
+    public class BaseClass
+    {
+        protected int HealthModifier = 50;
+
+        protected int Strength = 5;
+        protected int Defense = 5;
+        protected int Magic = 5;
+        protected int Resistance = 5;
+
+        private int SpeedModifier = 5;
+
+    }
+
+    class BlackMage : BaseClass
+    {
+        public BlackMage()
+        {
+            Strength = 2;
+            Magic = 10;
+            Defense = 2;
+            Resistance = 3;
+            
+        }
+    }
+ 
+}
