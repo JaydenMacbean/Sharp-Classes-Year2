9ed0f6ad95fe71d57a1f25356ffac106dd2c8ff6
Jayden <macbeanjayden@gmail.com>
2021-10-29 09:37:44 +0200

    les3 added

---
+++ Les3/Practice/Practice/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rect Cube = new Rect(4, 4, 4, 4);
+            Rect Square = new Rect(4, 4, 4);//Vierkant
+            Rect Thiccums = new Rect(100, 100);
+            Rect ThickSquare = new Rect(100);//vierkant
+            
+        }
+    }
+
+    public class Rect
+    {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+
+
+        public Rect(int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            
+            
+        }
+
+        public Rect(int x, int y, int size)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = size;
+            this.height = size;
+             
+
+             
+             
+             
+        }
+
+        public Rect(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public Rect(int size)
+        {
+            this.width = size;
+            this.height = size;
+        }
+
+        public bool SquareCheck()
+        {
+            if (width == height)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public void MoveTo(int x, int y)
+        {
+
+        }
+    }
+}
